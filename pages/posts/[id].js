f8d029c7ad52161a6c867d37b0f6035cb97c1d62
LordBigglesworth <javanpohl@gmail.com>
2021-03-23 13:55:00 -0600

    updated the blog-post links on the main page

---
+++ pages/posts/[id].js
@@ -0,0 +1,45 @@
+import Layout from '../../components/layout'
+import { getAllPostIds, getPostData } from '../../lib/posts'
+import Paper from '@material-ui/core/Paper'
+import utilStyles from '../../styles/utils.module.css'
+import Head from 'next/head'
+import Date from '../../components/date'
+
+// gets basic post data
+export async function getStaticProps({ params }) {
+  const postData = await getPostData(params.id)
+  return {
+    props: {
+      postData,
+    },
+  }
+}
+
+// gets the data to compile url paths for our pages
+export async function getStaticPaths() {
+  const paths = getAllPostIds()
+  return {
+    paths,
+    fallback: false,
+  }
+}
+
+export default function Post({ postData }) {
+  return (
+    <Layout>
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
+      <article>
+        <Paper className={utilStyles.blogBody} elevation={3}>
+        <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+          <div className={utilStyles.blogDate}>
+            <Date dateString={postData.date} />
+          </div>
+          <br />
+          <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+        </Paper>
+      </article>
+    </Layout>
+  )
+}
