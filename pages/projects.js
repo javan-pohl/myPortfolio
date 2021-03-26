919c99e2b635fc512788d46798464ed81e2c6797
LordBigglesworth <javanpohl@gmail.com>
2021-03-26 16:45:42 -0600

    updated date format on projects page

--- pages/projects.js
+++ pages/projects.js
@@ -3,6 +3,7 @@ import utilStyles from '../styles/utils.module.css'
 import { useRouter } from 'next/router'
 import { getSortedProjectsData } from '../lib/projects'
 import Link from 'next/link'
+import Date from '../components/date'
 
 export async function getStaticProps() {
   const allProjectsData = getSortedProjectsData()
