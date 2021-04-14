cb9bc16f9febf2a4abea6bb80f41e74c69b1882a
LordBigglesworth <javanpohl@gmail.com>
2021-04-14 11:46:14 -0600

    created a compressed version of the brick photo for the background url on the main page, changed the .main height element to 100vh, and removed the text-shadow elements for the main page links on mobile devices

--- components/layout.js
+++ components/layout.js
@@ -2,7 +2,6 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from './layout.module.css'
 import utilStyles from '../styles/utils.module.css'
-import Link from 'next/link'
 import NavBar from '../components/navBar'
 
 const name = 'Javan Pohl'
