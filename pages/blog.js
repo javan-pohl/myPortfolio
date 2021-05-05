4def6e1d10c9dd811daffad50ad92dc3e6c5d8a0
LordBigglesworth <javanpohl@gmail.com>
2021-05-05 14:05:52 -0600

    also added a filter function to getAllPostIds(), but this time, appending the filter to the map call via dot chaining

--- pages/blog.js
+++ pages/blog.js
@@ -19,18 +19,24 @@ export default function Home({ allPostsData }) {
   let thisPath = router.pathname.substr(1)
   return (
     <Layout current={thisPath}>
-
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-        <h2 className={utilStyles.headingLg} style={{margin: '15px 0 15px 0', paddingBottom: '15px'}}>Blog Posts:</h2>
+        <h2
+          className={utilStyles.headingLg}
+          style={{ margin: '15px 0 15px 0', paddingBottom: '15px' }}
+        >
+          Blog Posts:
+        </h2>
         <ul className={utilStyles.list}>
           {allPostsData.map(({ id, date, title }) => (
             <Link href={`/blog/posts/${id}`} key={id}>
-              <li className={utilStyles.listItem}>
-                <h2>{title}</h2>
-                <small className={utilStyles.lightText}>
-                  <Date dateString={date} />
-                </small>
-              </li>
+              <a>
+                <li className={utilStyles.listItem}>
+                  <h2>{title}</h2>
+                  <small className={utilStyles.lightText}>
+                    <Date dateString={date} />
+                  </small>
+                </li>
+              </a>
             </Link>
           ))}
         </ul>
